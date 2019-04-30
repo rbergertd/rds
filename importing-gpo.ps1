4af55b93c235fa764fdd7e3cb751db1853002490
Ryan Berger <ryan.berger@techdata.com>
2019-04-30 13:00:29 -0400

    Update importing-gpo.ps1

--- importing-gpo.ps1
+++ importing-gpo.ps1
@@ -1,22 +1,23 @@
-﻿#Building a new GPO, importing an existing configuration, and then linking it to the domain root.
+﻿#Building a new GPO, importing an existing configuration, and then linking it to the WVD Session Hosts OU.
 $LDAPRoot = [ADSI]"LDAP://RootDSE"
-$GPLinkTarget = $LDAPRoot.Get("rootDomainNamingContext")
+$GPLinkTargetDomain = $LDAPRoot.Get("rootDomainNamingContext")
 $URI = "http://github.com/rbergertd/rds/raw/master/grouppolicy/GPOBackup.zip"
+$WVDOU = "WVD Session Hosts"
+$GPLinkTarget = "ou=$WVDOU,"+($GPLinkTargetDomain)
 #Create Directory
 New-Item -ItemType "directory" -Path "C:\GPOBackup\"
 #Download GPO backup folder
-#Old way of forcing Tls version. The new way will automatically detect the appropriate version and use that one.
-#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -UseBasicparsing -Uri $URI -OutFile "C:\GPOBackup\GPOBackup.zip"
 #Unarchive .zip file
 Expand-Archive -LiteralPath C:\GPOBackup\GPOBackup.zip -DestinationPath C:\GPOBackup
 #Create OU on Domain
-New-ADOrganizationalUnit "RDS Session Hosts"
+New-ADOrganizationalUnit $WVDOU
 #Create GPO, import policy backup, and link to Domain root.
-new-gpo -name "Configure RDS Graphics" -Comment "Configure the appropriate GPO's for best graphic performance via RDP"
-import-gpo -backupid E802E58D-C478-4AB6-8437-830DF35761AA -TargetName "Configure RDS Graphics" -Path C:\GPOBackup
-new-gplink -name "Configure RDS Graphics" -Enforced Yes -LinkEnabled Yes -Target $GPLinkTarget
+new-gpo -name "ConfigureRDSGraphics" -Comment "Configure the appropriate GPO's for best graphic performance via RDP"
+import-gpo -backupid E802E58D-C478-4AB6-8437-830DF35761AA -TargetName "ConfigureRDSGraphics" -Path C:\GPOBackup
+new-gplink -name "ConfigureRDSGraphics" -Enforced Yes -LinkEnabled Yes -Target $GPLinkTarget  
+
 
 #Building a new GPO, importing an existing configuration, and then linking it to the RDS Session Hosts OU.
 $URI = "http://github.com/rbergertd/rds/raw/master/grouppolicy/GPOBackup.zip"
